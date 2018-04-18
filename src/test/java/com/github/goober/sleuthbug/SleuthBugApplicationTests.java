50e182435b961fc7e9a7f5d700345601befb08d0
Mathias Åhsberg <mathias.ahsberg@resurs.se>
2018-04-18 12:32:55 +0200

    Initial commit

---
+++ src/test/java/com/github/goober/sleuthbug/SleuthBugApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.goober.sleuthbug;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SleuthBugApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
